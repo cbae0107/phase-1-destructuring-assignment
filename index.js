0ed037df59d94948b81bf35756c5a1ff81be1ff8
Changhyun <chang.bae@flatironschool.com>
2023-03-07 14:11:10 -0800

    Destructuring

--- index.js
+++ index.js
@@ -25,24 +25,59 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+const sound = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken",
+}
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const moo = sound.moo
+const neigh = sound.neigh
+const baa = sound.baa
+const oink = sound.oink
+const cluck = sound.cluck
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const [bessie, horse, dolly, babe, little] = farmAnimals.split(" ");
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [blackAndWhite, brown, black, pink, white] = farmAnimals.split(" ")
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const red = "red"
+const orange = "orange"
+const yellow = "yellow"
+const green = "green"
+const blue = "blue"
+const indigo = "indigo"
+const violet = "violet"
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, i, v] = colors
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const indg = indigo 
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const muppetName = "Miss Piggy"
+const color = 'pink'
+const song = 'Never Before, Never Again'
+const job = 'Cast member of The Muppet Show'
+const partner = 'Kermit'
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const song2 = "Moving Right Along"
+const song4 = "I Hope That Something Better Comes Along"
+const nestedJob = 'Host of The Muppet Show'
+const nestedPartner = 'Miss Piggy'
+
+
